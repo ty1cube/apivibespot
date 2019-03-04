a625c600edf655d604333b5ec2993a3bb0cecd88
Azeez Akinsola <harkzed@gmail.com>
2019-03-04 13:29:58 +0100

    edit the view

--- authentications/const_views.py
+++ authentications/const_views.py
@@ -25,9 +25,9 @@ class LocalAreaView(views.APIView):
     permission_classes = (permissions.AllowAny,)
 
     def get(self, request, format=None):
-        # object = const_models.LocalArea.objects.all()
+       object = const_models.LocalArea.objects.all()
         # object = const_models.LocalArea.objects.filter(localareaview_set__state__id=id)
-        object = State.objects.filter(state=request.GET['state_id'])
+        # object = State.objects.filter(state=request.GET['state_id'])
 
         serializer = const_serializers.LocalAreaSerializer(object, many=True)
         return Response(serializer.data)
